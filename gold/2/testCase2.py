1c5377071a91d12a19d8ba097b83c54f72b01276
Pratham <62070812+prathami1@users.noreply.github.com>
2021-04-15 21:10:16 -0400

    GOLD 🏆

--- gold/2/testCase2.py
+++ gold/2/testCase2.py
@@ -4,5 +4,5 @@ N,L = map(int,input().split())
 C1, C2, C3, C4 = map(int,input().split())
 
 for i in range(input):
-  true = check(C1 = C2 + C3 AND C2 = C3 + C4 OR C3 = C4 + C1)
+  true = check(C1 = C2 + C3 OR C2 = C3 + C4 OR C3 = C4 + C1)
   print(true)
